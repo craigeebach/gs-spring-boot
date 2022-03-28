bcabc91190ad82227db7ea16a8ee6312381f711d
Craig Reichenbach <craig_reichenbach@us.ibm.com>
2022-03-28 12:25:15 -0400

    adds endpoints

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -1,14 +1,53 @@
 package com.example.springboot;
 
+import com.github.javafaker.Faker;
+import com.google.gson.Gson;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "GS Spring Boot (Azure Quickstart 2) Index page";
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(required = false) String name) {
+		if (name == null) {
+			return "Hello from Azure Quickstart 2";
+		} else {
+			return "Hello from Azure Quickstart 2 "+name;
+		}
+	}
+
+	@GetMapping("/greetings/{name}")
+	public String greetings(@PathVariable("name") String name) {
+		return "Greetings from Azure Quickstart 2 "+name;
+	}
+
+	@GetMapping("/person")
+	public Person person(@RequestParam(required = false) String name) {
+		Faker faker = new Faker();
+		int age = (int) (Math.random() * (100 - 1)) + 1;
+		Person p = new Person(faker.name().fullName(), age);
+		log.info("Returning new person : {}", new Gson().toJson(p));
+		return p;
+	}
+
+	@Data
+	static class Person {
+		
+		private final String name;
+		private final int age;
 	}
 
 }
